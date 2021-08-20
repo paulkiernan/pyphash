274b4c79c08f41dd2444cd6fea79f12a0a4c39e8
Michael T. Babcock <mtb-coder-git@mikebabcock.ca>
2021-08-20 18:06:52 -0400

    Upgraded to Python3 and removed redundant MD5 hashing

--- test_hashing.py
+++ test_hashing.py
@@ -4,74 +4,58 @@
 """ phashlib Core C++ interface
 """
 
-# Future-proof
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
-# Standard Library Imports
-from hashlib import md5
-from itertools import imap
-
 # Third-party Libraries
 import matplotlib.pyplot as plt
 from IPython.display import Image, display
 
 # Custom libphash bindings
-import python_phash as phash
+import pyphash as phash
 print(phash)
 print(dir(phash))
 
 
 TEST_IMAGES = {
-    'base': 'test_images/tony.png',
+    'base': 'test_images/sample.png',
     'blurs': {
-        '2x2': 'test_images/tony-blur-2x2.png',
-        '5x2': 'test_images/tony-blur-5x2.png',
-        '0x4': 'test_images/tony-blur-0x4.png',
+        '2x2': 'test_images/sample-blur-2x2.png',
+        '5x2': 'test_images/sample-blur-5x2.png',
+        '0x4': 'test_images/sample-blur-0x4.png',
     },
     'rotations': {
-        '10°': 'test_images/tony-rotate-10.png',
-        '30°': 'test_images/tony-rotate-30.png',
-        '90°': 'test_images/tony-rotate-90.png',
-        '180°': 'test_images/tony-rotate-180.png',
+        '10°': 'test_images/sample-rotate-10.png',
+        '30°': 'test_images/sample-rotate-30.png',
+        '70°': 'test_images/sample-rotate-70.png',
+        '90°': 'test_images/sample-rotate-90.png',
     },
     'scaled': {
-        '50%': 'test_images/tony-scaled-50.png',
-        '75%': 'test_images/tony-scaled-75.png',
-        '150%': 'test_images/tony-scaled-150.png',
+        '50%': 'test_images/sample-scaled-50.png',
+        '75%': 'test_images/sample-scaled-75.png',
+        '150%': 'test_images/sample-scaled-150.png',
     }
 }
 
 # Helpers
 def hamming_distance(string1, string2):
-    return sum(imap(str.__ne__, string1, string2))
+    return sum(map(str.__ne__, string1, string2))
 
 
 def test_image_correlations(image_distortion_type, is_ipython_notebook=False):
-
     base_image_path = TEST_IMAGES.get('base')
     base_image = Image(filename=base_image_path)
     base_phash_digest = phash.image_digest(base_image_path)
     base_phash_imagehash = phash.imagehash(base_image_path)
-    base_md5_digest = md5(open(base_image_path).read()).hexdigest()
 
     if is_ipython_notebook:
         display(base_image)
-    print("md5 Digest: {0}".format(base_md5_digest))
     print("pHash Digest: {0}".format(base_phash_digest))
     print("pHash DCT Hash: {0}".format(base_phash_imagehash))
 
-    for distortion_info, image_path in TEST_IMAGES.get(image_distortion_type).iteritems():
+    for distortion_info, image_path in TEST_IMAGES.get(image_distortion_type).items():
         _image = Image(filename=image_path)
         if is_ipython_notebook:
             display(_image)
         _image_phash_digest = phash.image_digest(image_path)
         _image_phash_imagehash = phash.imagehash(image_path)
-        _image_md5_digest = md5(open(image_path).read()).hexdigest()
-        print("md5 Digest: {0}".format(_image_md5_digest))
-        print("md5 Hamming Distance: {0}".format(hamming_distance(base_md5_digest, _image_md5_digest)))
         print("pHash Digest: {0}".format(_image_phash_digest))
         print("pHash DCT Hash: {0}".format(_image_phash_imagehash))
         print("pHash DCT Hash Hamming Distance: {0}".format(
@@ -88,25 +72,25 @@ def test_image_correlations(image_distortion_type, is_ipython_notebook=False):
 
 
 def test_rotations():
-
     base_image_path = TEST_IMAGES.get('base')
     base_phash_digest = phash.image_digest(base_image_path)
 
     correlations = []
-    for x in xrange(1, 361):
-        _image_path = "test_images/tony-rotate-{0}.png".format(x)
+    for x in range(1, 90):
+        _image_path = "test_images/sample-rotate-{0}.png".format(x)
         _image_phash_digest = phash.image_digest(_image_path)
         correlations.append((
             x,
             phash.cross_correlation(base_phash_digest, _image_phash_digest)
         ))
 
-    return zip(*correlations)
+    return list(zip(*correlations))
 
-test_image_correlations('scaled', is_ipython_notebook=False)
-test_image_correlations('blurs', is_ipython_notebook=False)
-test_image_correlations('rotations', is_ipython_notebook=False)
-xx, yy = test_rotations()
-plt.plot(xx, yy)
-plt.plot(xx[:10], yy[:10])
-plt.plot(xx[-10:], yy[-10:])
+if __name__ == "__main__":
+    test_image_correlations('scaled', is_ipython_notebook=False)
+    test_image_correlations('blurs', is_ipython_notebook=False)
+    test_image_correlations('rotations', is_ipython_notebook=False)
+    xx, yy = test_rotations()
+    plt.plot(xx, yy)
+    plt.plot(xx[:10], yy[:10])
+    plt.plot(xx[-10:], yy[-10:])
